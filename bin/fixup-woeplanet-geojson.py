9436b391915d817214e349fb466550f47e01124c
Gary Gale <gary@vicchi.org>
2020-12-20 13:05:14 +0000

    fix: update missing properties in WoePlanet GeoJSON; add GeoJSON to ES loader script

--- bin/fixup-woeplanet-geojson.py
+++ bin/fixup-woeplanet-geojson.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+import datetime
 import geojson
+import hashlib
 import json
 import logging
 import os
+import pyproj
+import shapely.geometry
 import sys
 
 class Fixer(object):
@@ -19,33 +23,148 @@ class Fixer(object):
                 for file in files:
                     if '.geojson' in file:
                         srcfile = os.path.join(root, file)
-                        self.fixup_geojson(srcfile)
-                        self.validate_geojson(srcfile)
+                        self.do_fixups(srcfile)
 
         else:
-            self.fixup_geojson(path)
-            self.validate_geojson(srcfile)
+            self.do_fixups(path)
 
-    def fixup_geojson(self, path):
+    def do_fixups(self, path):
         with open(path, 'r') as reader:
-            doc = json.load(reader)
+            try:
+                doc = json.load(reader)
+            except Exception as e:
+                logging.error('%s: %s' % (path, e))
+                raise e
 
+            srchash = hashlib.md5(json.dumps(doc, sort_keys=True).encode('utf8')).hexdigest()
+
+            doc = self.fixup_scale(doc)
+            doc = self.fixup_geometry(doc, path)
+            doc = self.fixup_area(doc, path)
+            self.validate_geojson(doc, path)
+
+            hash = hashlib.md5(json.dumps(doc, sort_keys=True).encode('utf8')).hexdigest()
+            if srchash != hash:
+                doc['properties']['meta:updated'] = str(datetime.datetime.utcnow().isoformat())
+                logging.debug('Writing update to %s' % path)
+                with open(path, 'w') as writer:
+                    json.dump(doc, writer)
+
+    def fixup_geometry(self, doc, path):
         woeid = int(doc['properties']['woe:id'])
         if 'geometry' in doc:
+            logging.debug('Checking geometry for %s' % path)
             if doc['geometry']['type'] not in ['Point', 'Polygon', 'MultiPolygon']:
                 logging.warning('%s: fixing unexpected geometry type %s in %s' % (woeid, doc['geometry']['type'], path))
                 if doc['geometry']['type'] == 'point':
                     doc['geometry']['type'] = 'Point'
 
-                    with open(path, 'w') as writer:
-                        json.dump(doc, writer)
+        else:
+            doc['geometry'] = {
+                'type': 'Point',
+                'coordinates': [0.0, 0.0]
+            }
 
-    def validate_geojson(self, path):
-        with open(path, 'r') as reader:
-            doc = geojson.load(reader)
-        
-        woeid = int(doc['properties']['woe:id'])
-        if not doc.is_valid:
+        doc = self.fixup_coords(doc, 'woe:latitude')
+        doc = self.fixup_coords(doc, 'woe:longitude')
+        doc = self.fixup_coords(doc, 'geom:latitude')
+        doc = self.fixup_coords(doc, 'geom:longitude')
+        doc = self.fixup_centroid(doc, 'woe:centroid')
+        doc = self.fixup_centroid(doc, 'geom:centroid')
+
+        return doc
+
+    def fixup_coords(self, doc, property):
+        if not property in doc['properties']:
+            doc['properties'][property] = 0.0
+        return doc
+
+    def fixup_centroid(self, doc, property):
+        if not property in doc['properties']:
+            doc['properties'][property] = [0.0, 0.0]
+        return doc
+
+    def fixup_area(self, doc, path):
+        if 'geometry' in doc:
+            logging.debug('Checking geom:area for %s' % path)
+            geodoc = geojson.loads(json.dumps(doc))
+            geod = pyproj.Geod(ellps='WGS84')
+            geom = shapely.geometry.shape(geodoc['geometry'])
+            geodoc['properties']['geom:area'] = abs(geod.geometry_area_perimeter(geom)[0])
+
+        return geodoc
+
+    def fixup_scale(self, doc):
+        ptid = int(doc['properties']['woe:placetype'])
+        scale = 0
+        if ptid == 19:  # supername
+            scale = 1
+        elif ptid == 29:  # continent
+            scale = 2
+        elif ptid == 38:  # sea
+            scale = 3
+        elif ptid == 37:    # ocean
+            scale = 3
+        elif ptid == 21:  # region
+            scale = 4
+        elif ptid == 12:  # country
+            scale = 5
+        elif ptid == 8:  # state
+            scale = 6
+        elif ptid == 18:  # nationality
+            scale = 7
+        elif ptid == 31:  # timezone
+            scale = 8
+        elif ptid == 9:  # county
+            scale = 9
+        elif ptid == 36:    # aggregate
+            scale = 10
+        elif ptid == 13:  # island
+            scale = 11
+        elif ptid == 16:  # land feature
+            scale = 11
+        elif ptid == 24:  # colloquial
+            scale = 12
+        elif ptid == 10:  # local admin
+            scale = 13
+        elif ptid == 25:  # zone
+            scale = 14
+        elif ptid == 15:  # drainage
+            scale = 15
+        elif ptid == 7:  # town
+            scale = 16
+        elif ptid == 22:  # suburb
+            scale = 17
+        elif ptid == 33:  # estate
+            scale = 18
+        elif ptid == 23:  # sports team
+            scale = 19
+        elif ptid == 20:  # poi
+            scale = 19
+        elif ptid == 14:  # airport
+            scale = 19
+        elif ptid == 17:  # miscellaneous
+            scale = 20
+        elif ptid == 6:  # street
+            scale = 21
+        elif ptid == 32:  # nearby intersection
+            scale = 21
+        elif ptid == 11:  # zip
+            scale = 22
+        elif ptid == 26:  # historical state
+            scale = 23
+        elif ptid == 27:  # historical county
+            scale = 24
+        elif ptid == 35:  # historical town
+            scale = 25
+
+        doc['properties']['woe:scale'] = scale
+        return doc
+
+    def validate_geojson(self, doc, path):
+        geodoc = geojson.loads(json.dumps(doc))
+        if not geodoc.is_valid:
+            woeid = int(geodoc['properties']['woe:id'])
             raise Exception('%s: invalid GeoJSON found in %s' % (woeid, path))
 
 def main():
@@ -71,28 +190,6 @@ def main():
 
         except Exception as e:
             logging.error(e)
-
-
-    # try:
-    #     for path in args.paths:
-    #         if not os.path.exists(path):
-    #             logging.error('%s: no such file or directory' % path)
-
-    #         elif os.path.isdir(path):
-    #             for root, _, files in os.walk(path):
-    #                 for file in files:
-    #                     if '.geojson' in file:
-    #                         srcfile = os.path.join(root, file)
-    #                         index_geojson(srcfile, args)
-
-    #         else:
-    #             index_geojson(path, args)
-
-    # except Exception as e:
-    #     logging.error(e)
-
-# def index_geojson(path, args):
-#     pass
 
 if __name__ == '__main__':
     import argparse
